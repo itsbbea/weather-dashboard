cd4d93efe4494701156e5ec5664ac9d677f67d70
itsbbea <bea.may27@hotmail.com>
2023-10-11 00:08:38 +1100

    added open weather API key

--- assets/script.js
+++ assets/script.js
@@ -5,6 +5,10 @@ var forecast = document.getElementByClass("forecast")
 function getForecast() {
     var inputText = document.getElementById("input").value.trim();
     saveSearchHistory(inputText)
-    fetch()
+    fetch(api.openweathermap.org/data/2.5/forecast?lat={lat}&lon:{lon}&appid,{API,key}).then(function (input) {
+
+
+    })
+
 
 }
