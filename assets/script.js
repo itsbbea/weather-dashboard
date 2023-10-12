e00b4dfb3bd6e5a74e17b27eb44afa304d90db4b
itsbbea <bea.may27@hotmail.com>
2023-10-12 12:16:03 +1100

    modified script.js, added API key

--- assets/script.js
+++ assets/script.js
@@ -5,7 +5,7 @@ var forecast = document.getElementByClass("forecast")
 function getForecast() {
     var inputText = document.getElementById("input").value.trim();
     saveSearchHistory(inputText)
-    fetch(api.openweathermap.org/data/2.5/forecast?lat={lat}&lon:{lon}&appid,{API,key}).then(function (input) {
+    fetch("https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}").then(function (input) {
 
 
     })
